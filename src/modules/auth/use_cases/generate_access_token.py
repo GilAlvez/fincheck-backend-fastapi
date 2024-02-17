9883f9009685acfd5a33597f2a92fdf1b3b5d39c
GilAlvez <gildson_af@outlook.com>
2024-02-17 15:38:28 -0300

    feat: implements generate access token use case

--- src/modules/auth/use_cases/generate_access_token.py
+++ src/modules/auth/use_cases/generate_access_token.py
@@ -1,6 +1,28 @@
+import jwt
+from datetime import datetime, timedelta
 from src.modules.auth.entities.user import User
 
 
 class GenerateAccessToken:
+    def __init__(
+        self,
+        secret_key: str,
+        algorithm: str = "HS256",
+        expires_in: timedelta = timedelta(days=2),
+    ):
+        self.secret_key = secret_key
+        self.algorithm = algorithm
+        self.expires_in = expires_in
+
     def execute(self, user: User) -> str:
-        return "token"
+        payload = {
+            "sub": user.id,
+            "iat": datetime.utcnow(),
+            "exp": datetime.utcnow() + self.expires_in,
+        }
+
+        return jwt.encode(
+            key=self.secret_key,
+            algorithm=self.algorithm,
+            payload=payload,
+        )
